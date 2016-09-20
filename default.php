70ea8d21b7ace713bef82754b0f8939ca2827880
Dan Cojocaru <dancojocaru2010@hotmail.com>
2016-09-20 18:51:19 +0300

    Further bug fix
    Bux fix related to absolute web host path

--- default.php
+++ default.php
@@ -7,24 +7,24 @@
 <html manifest="orar9e.appcache">
 	<head>
 		<?php
-			require '/home/u500659049/public_html/index/head.php';
+			require '/home/u567331873/public_html/index/head.php';
 		?>
 	</head>
 	<body style="font-family: 'Ubuntu', sans-serif;">
 		<div id="wrapper">
 			<div id="header">
 				<script language="php">
-					require '/home/u500659049/public_html/index/header.php';
+					require '/home/u567331873/public_html/index/header.php';
 				</script>
 			</div>
 			<div id="content" style="color:black;">
 				<script language="php">
-					if ($_GET["table"]==1) require '/home/u500659049/public_html/index/tabel1.php';
+					if ($_GET["table"]==1) require '/home/u567331873/public_html/index/tabel1.php';
 				</script>
 			</div>
 			<div id="footer">
 				<script language="php">
-					require '/home/u500659049/public_html/index/footer_index.php';
+					require '/home/u567331873/public_html/index/footer_index.php';
 				</script>
 			</div>
 		</div>
